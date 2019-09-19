4c2c70b16ecd1771c0d2fb6d98cdee65c2408b40
Neal Cynkus <Neal.Cynkus@gmail.com>
2019-09-18 23:09:56 -0600

    add a modal and drawer.  add foneawesome pro (requires a license in npmrc)

--- Vuetemplate/ClientApp/main.js
+++ Vuetemplate/ClientApp/main.js
@@ -5,6 +5,27 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Modal from './components/modal/Modal.vue';
+import Drawer from './components/Drawer.vue';
+
+Vue.component('modal', Modal);
+Vue.component(Drawer.name, Drawer);
+
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome';
+
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('font-awesome-layers', FontAwesomeLayers);
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText);
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimes } from '@fortawesome/pro-regular-svg-icons';
+import { faEllipsisVAlt } from '@fortawesome/pro-regular-svg-icons';
+
+library.add(faTimes);
+library.add(faEllipsisVAlt);
+
+
 
 
 
--- Vuetemplate/ClientApp/views/Home.vue
+++ Vuetemplate/ClientApp/views/Home.vue
@@ -1,14 +1,56 @@
 ﻿<template>
     <div class="row">
-        <div class="offset-2 col-8 text-center">
-            <p>Hello World!</p>
-            <img src="../assets/500px-Vue.js_Logo_2.svg.png" alt="vue logo" />
+        <div class="col-2">
+            <drawer v-show="showDrawer" @close-drawer="showDrawer = false" key="drawer" title="Navigation Drawer">
+                A drawer!
+            </drawer>
+            <transition name="leftSlide" mode="in-out">
+                <div class="overlay" key="overlay" v-show="showDrawer"></div>
+            </transition>
+            
+        </div>
+        <div class="col-md-8 col-12 text-center">
+            <p @click="showDrawer = true">Hello World!</p>
+            
+            <modal v-show="showModal" @close="showModal=false">
+                <template v-slot:header>
+                    Header
+                </template> 
+                <template v-slot:body>
+                    <div>
+                        This is a body
+                        <img src="../assets/500px-Vue.js_Logo_2.svg.png" alt="vue logo" @click="showModal=true" />
+                    </div>
+                </template>
+                
+            </modal>
+            <img src="../assets/500px-Vue.js_Logo_2.svg.png" alt="vue logo" @click="showModal=true" />
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'home',        
+        name: 'home',
+        data() {
+            return {
+                showModal: false,
+                showDrawer: false
+            }
+        }
     };
 </script>
+
+<style lang="scss">
+    .overlay{
+        display:block;
+        position:fixed;
+        top:0;
+        width:100vw;
+        height:100vh;
+
+        background: rgba(0,0,0,0.7);
+        z-index:1100;
+        opacity:1;
+    }
+</style>
